13d8cfd2a1aad6f164b81ecd970494043446a5fc
pitu <ashishkumarvish.432@gmail.com>
2021-10-03 21:54:22 +0530

    Reverse no.

--- src/org/BridgeLabz/Logical_Pro.java
+++ src/org/BridgeLabz/Logical_Pro.java
@@ -2,21 +2,14 @@ package org.BridgeLabz;
 import java.util.Scanner;
 public class Logical_Pro {
     public static void main(String[] args) {
-        int n, count=0;
+     int n,res;
+        System.out.println("Enter any Number");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
         n = sc.nextInt();
-        for(int i =1; i<=n; i++) {
-            if(n%i==0){
-                count = count++;
-            }
-             if(count==2){
-
-                System.out.println("Number is the Prime Number");
-            }
-            else {
-                System.out.println("Number is not Prime Number");
-            }
+        while(n>0){
+            res = n%10;
+            System.out.println("Reverse nuber" +res);
+            n  = n/10;
         }
 
     }
