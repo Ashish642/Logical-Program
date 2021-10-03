5ec536244c30a21fb10a4bc60176162e0b42b4c0
pitu <ashishkumarvish.432@gmail.com>
2021-10-03 17:22:12 +0530

    Print fibo.series

--- src/org/BridgeLabz/Logical_Pro.java
+++ src/org/BridgeLabz/Logical_Pro.java
@@ -1,7 +1,16 @@
 package org.BridgeLabz;
-
+import java.util.Scanner;
 public class Logical_Pro {
     public static void main(String[] args) {
-        System.out.println("Welcome t to logical program");
+        Scanner sc = new Scanner(System.in);
+        int x = 0 , y = 1, sum = 0, n;
+        System.out.println("Enter the valu ");
+        n = sc.nextInt();
+        for( int i=0; i<=n; i++){
+            System.out.println(sum);
+            x = y;
+            y = sum;
+            sum = x+y;
+        }
     }
 }
