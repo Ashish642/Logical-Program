054fd64bf8059866574d5e09add630db04f6e07b
pitu <ashishkumarvish.432@gmail.com>
2021-10-03 22:32:05 +0530

    stopwatch

--- src/org/BridgeLabz/Logical_Pro.java
+++ src/org/BridgeLabz/Logical_Pro.java
@@ -2,15 +2,17 @@ package org.BridgeLabz;
 import java.util.Scanner;
 public class Logical_Pro {
     public static void main(String[] args) {
-     int n,res;
-        System.out.println("Enter any Number");
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        while(n>0){
-            res = n%10;
-            System.out.println("Reverse nuber" +res);
-            n  = n/10;
-        }
+     Scanner sc = new Scanner(System.in);
+        System.out.println("Tye 'r' to start: ");
+        long start = System.currentTimeMillis();
+        sc.nextLine();
+        System.out.println("Type ;s; to stop: ");
+        sc.nextLine();
+        long stop = System.currentTimeMillis();
+        long ElapsedTime = (stop - start ) / 1000;
+        System.out.println("Elapsed Time was : " +ElapsedTime+ "Second");
+
+
 
     }
 }
